7b066ff620a02f1b0a0ded171a4dcc80779c142c
solaikulola <israelaikulola4@gmail.com>
2021-04-19 10:31:10 -0400

    added listing descibe

--- model/process.py
+++ model/process.py
@@ -4,4 +4,6 @@ import pandas as pd
 listing = pd.read_csv("https://raw.githubusercontent.com/israelaikulola/worksample/main/Data/listings.csv")
 print(listing.shape)
 print(listing.head(2))
+print(listing.info())
+listing.describe()
 
